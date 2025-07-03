13fc7849d7309ca4375fda8950eaecf8575de834
Yuan Chiu <chyuaner@gmail.com>
2025-07-04 05:23:46 +0800

    index animate

--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -1,7 +1,32 @@
 export function initPage() {
-  gsap.from('h1', {
-    scale: 0.8,
+  const tl = gsap.timeline();
+
+  tl
+	.from('#main-avatar', {
+    x: -200,
+    scale: 0.9,
+    opacity: 0,
+    duration: 0.8
+  })
+	.from('#main-content', {
     opacity: 0,
     duration: 0.5
+  })
+	// .from('#site-title', {
+  //   scale: 0.8,
+  //   opacity: 0,
+  //   duration: 0.5
+  // })
+  .from('#contact-links', {
+    // scale: 0.8,
+		y: -10,
+    opacity: 0,
+    duration: 0.3
+  })
+  .from('#main-links', {
+    // scale: 0.8,
+		y: -10,
+    opacity: 0,
+    duration: 0.3
   });
 }
